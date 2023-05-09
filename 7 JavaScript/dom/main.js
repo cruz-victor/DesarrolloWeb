91c3a551b7980d4c6f4bdd3e9ca18802137ebc1a
Vic <cruz.gomez.victor@gmail.com>
2023-05-09 14:04:31 -0400

    Dom, getElemensByTagName, getElementsByClassName, getElementById

---
+++ 7 JavaScript/dom/main.js
@@ -0,0 +1,16 @@
+//BY TAGNAME
+// var cajas=document.getElementsByTagName('div');
+// console.log('Cajas:');
+// console.log(cajas);
+
+// console.log('cajas---');
+// console.log(cajas[0]);
+// // cajas[0].textContent = 'Hola victor33';
+// cajas[0].innerHTML='<h1>hola</h1>';
+
+
+//BY CLASSNAME
+// var cajas=document.getElementsByClassName('caja')
+
+//BY ID
+var primeraCaja=document.getElementById('primeraCaja');
---
+++ 7 JavaScript/dom/mainv2.js
@@ -0,0 +1,14 @@
+//BY TAGNAME
+// var cajas =document.getElementsByTagName('div');
+// console.log(cajas);
+// console.log(cajas[0]);
+
+//BY CLASSNAME
+// var cajas=document.getElementsByClassName('caja');
+// console.log(cajas);
+// console.log(cajas[0]);
+
+//BY ID
+var caja=document.getElementById('primeraCaja');
+console.log(caja);
+caja.textContent = 'Hola Victor'
