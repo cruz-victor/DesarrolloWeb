b98f0154ed7c5883d116239e377414eb8d792b5b
Vic <cruz.gomez.victor@gmail.com>
2023-05-29 13:03:37 -0400

    Inyeccion entre servicios

--- 11 Angular/listado-personas-app/src/app/personasService.service.ts
+++ 11 Angular/listado-personas-app/src/app/personasService.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from "@angular/core";
 import { Persona } from "./persona.model";
+import { LoggingService } from "./loggingService.service";
 
-
+@Injectable()
 export class PersonasService{
   personas:Persona[]=[
     new Persona('Victor','CG'),
     new Persona('Grace','CG'),
     new Persona('Elon','CG')];
 
+  constructor(private loggingService:LoggingService) {}
+
+
   agregarPersona(persona:Persona){
+    this.loggingService.enviaMensajeAconsoloca("agregamos a una personas: "+persona.nombre);
     this.personas.push(persona);
   }
 }
