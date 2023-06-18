04a5591b5efc1a75124387c9cf4a42862c2ad4d3
Vic <cruz.gomez.victor@gmail.com>
2023-06-18 14:20:35 -0400

    Se agreto el archivo de rutas AppRoutingModule

--- 11 Angular/listado-personas-app/src/app/app.module.ts
+++ 11 Angular/listado-personas-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { PersonaComponent } from './persona/persona.component';
@@ -18,7 +18,8 @@ import { PersonasService } from './personasService.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [LoggingService, PersonasService],
   bootstrap: [AppComponent]
