908ab6661241c97063a0c85e2606a826c2b33ca5
Vic <cruz.gomez.victor@gmail.com>
2023-06-18 16:44:56 -0400

    Se implemento el paso de parametros y la modificacion de personas

--- 11 Angular/listado-personas-app/src/app/personas/formulario/formulario.component.ts
+++ 11 Angular/listado-personas-app/src/app/personas/formulario/formulario.component.ts
@@ -1,32 +1,36 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+  OnInit,
+} from '@angular/core';
 import { Persona } from '../../persona.model';
 import { outputAst } from '@angular/compiler';
 import { LoggingService } from '../../loggingService.service';
 import { PersonasService } from '../../personasService.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css'],
   //providers:[LoggingService] //Inyeccion de dependencia
-
 })
 export class FormularioComponent {
-
   // @Output()
   // personaCreada=new EventEmitter<Persona>();
 
-
-  nombreInput:string='';
-  apellidoInput:string='';
+  nombreInput: string = '';
+  apellidoInput: string = '';
+  i: number;
 
   // @ViewChild('nombreInput')
   // nombreInput:ElementRef;
 
   // @ViewChild('apellidoInput')
   // apellidoInput:ElementRef;
-
 
   // agregarPersona(){
   //   let persona1=new Persona(this.nombreInput,this.apellidoInput);
@@ -39,12 +43,15 @@ export class FormularioComponent {
   // }
 
   //Inyeccion de dependencia
-  constructor(private loggingService:LoggingService,
-              private personasService:PersonasService,
-              private router:Router){
-                this.personasService.saludar.subscribe(
-                  (i:number)=> alert("El indici es:"+i)
-                );
+  constructor(
+    private loggingService: LoggingService,
+    private personasService: PersonasService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.personasService.saludar.subscribe((i: number) =>
+      alert('El indici es:' + i)
+    );
   }
 
   // agregarPersona(){
@@ -54,10 +61,27 @@ export class FormularioComponent {
   //   //this.personaCreada.emit(persona1);
   // }
 
-  onGuardarPersona(){
-    let persona1=new Persona(this.nombreInput, this.apellidoInput);
-    this.loggingService.enviaMensajeAconsoloca("Enviamos persona:"+persona1.nombre);
-    this.personasService.agregarPersona(persona1);
+  ngOnInit() {
+    this.i = this.route.snapshot.params['id'];
+    if(this.i){ //Si el indice es diferente de nulo, entonces editar
+      let persona:Persona=this.personasService.encontrarPersona(this.i);
+      this.nombreInput=persona.nombre;
+      this.apellidoInput=persona.apellido;
+    }
+  }
+
+  onGuardarPersona() {
+    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+    // this.loggingService.enviaMensajeAconsoloca(
+    //   'Enviamos persona:' + persona1.nombre
+    // );
+
+    if (this.i) { //modo edicion
+      this.personasService.modificarPersona(this.i, persona1);
+    }else{
+      this.personasService.agregarPersona(persona1);
+    }
+
     this.router.navigate(['personas']);
   }
 }
--- 11 Angular/listado-personas-app/src/app/personasService.service.ts
+++ 11 Angular/listado-personas-app/src/app/personasService.service.ts
@@ -17,4 +17,17 @@ export class PersonasService{
     this.loggingService.enviaMensajeAconsoloca("agregamos a una personas: "+persona.nombre);
     this.personas.push(persona);
   }
+
+  encontrarPersona(i: number) {
+    let persona:Persona=this.personas[i];
+    return persona;
+  }
+
+  modificarPersona(i: number, persona: Persona) {
+    let persona2:Persona=this.personas[i];
+    persona2.nombre=persona.nombre;
+    persona2.apellido=persona.apellido;
+    this.personas[i]=persona2; //Esta linea se puede oviar por que los datos son pasados por referencia
+  }
+
 }
