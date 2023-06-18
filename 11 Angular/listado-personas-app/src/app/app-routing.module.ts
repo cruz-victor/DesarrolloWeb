56b2390a660348b54704319f22a3f70455786125
Vic <cruz.gomez.victor@gmail.com>
2023-06-18 19:44:52 -0400

    Se agrego un componente de error

--- 11 Angular/listado-personas-app/src/app/app-routing.module.ts
+++ 11 Angular/listado-personas-app/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PersonasComponent } from './personas/personas.component';
 import { FormularioComponent } from './personas/formulario/formulario.component';
+import { ErrorComponent } from './error/error.component';
 
 //Variables de rutas
 // const routes:Routes = [
@@ -16,7 +17,8 @@ const routes:Routes = [
   {path:'personas', component:PersonasComponent, children:[
     {path:'agregar', component:FormularioComponent},
     {path:':id', component:FormularioComponent}
-  ]}
+  ]},
+  {path:'**', component:ErrorComponent}
 ]
 
 @NgModule({
